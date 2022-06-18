94946d26f519b5ba4bb487b3f8fdd9df4e9b28e2
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-06-18 15:05:04 +0900

    fix: prettier error

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,44 +1,49 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const proggressApi = "progress"
+const proggressApi = 'progress'
 const instance = axios.create({
-  baseURL: "https://api.deathmatv.online/",
-});
+  baseURL: 'https://api.deathmatv.online/',
+})
 
-interface Progress  {
-    count: number,
-    open_cnt: number,
-    progress: number
+interface Progress {
+  count: number
+  open_cnt: number
+  progress: number
 }
 
-const intervalMs = 1000;
+const intervalMs = 1000
 
 const ProgressBar = () => {
-    const [count, setCount] = useState(0);
-    const [progress, setProgress] = useState({ count: 0, open_cnt: 0, progress: 0 });
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            instance.get(proggressApi).then(response => {
-                setProgress(JSON.parse(response.data));
-                setCount(count + 1);
-                
-                console.log(count + " " + response.data);
-            })                               //成功した場合、postsを更新する（then）
-                .catch(() => {
-                    console.log('通信に失敗しました');
-                });
-        }, intervalMs);
-        
-        return () => {
-            clearInterval(intervalId)
-        };
+  const [count, setCount] = useState(0)
+  const [progress, setProgress] = useState({
+    count: 0,
+    open_cnt: 0,
+    progress: 0,
+  })
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      instance
+        .get(proggressApi)
+        .then((response) => {
+          setProgress(JSON.parse(response.data))
+          setCount(count + 1)
 
-    }, [progress]);
+          console.log(count + ' ' + response.data)
+        }) //成功した場合、postsを更新する（then）
+        .catch(() => {
+          console.log('通信に失敗しました')
+        })
+    }, intervalMs)
 
-    return (
-        <progress id="issue_progress" max="1" value={progress.progress}></progress>
-    )
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [progress])
+
+  return (
+    <progress id="issue_progress" max="1" value={progress.progress}></progress>
+  )
 }
 
 export default ProgressBar
