fd9c7f73b245b4c9c8a3fc9dcf8898ee90f41f7f
i15317 <30828280+notchman8600@users.noreply.github.com>
2022-06-18 16:47:51 +0900

    オレオレ認証曲を追加

--- src/components/AmazonIVSPlayer.tsx
+++ src/components/AmazonIVSPlayer.tsx
@@ -1,59 +1,95 @@
-import { PlayerConfig, registerIVSTech } from 'amazon-ivs-player'
-import React, { useEffect, useRef } from 'react'
-import videojs, { VideoJsPlayer } from 'video.js'
+import { useEffect, useRef } from 'react'
+
 import 'video.js/dist/video-js.css'
 
 const AmazonIVSPlayer = () => {
-  const playerRef = useRef<VideoJsPlayer>()
-
+  // const playerRef = useRef<VideoJsPlayer>()
+  const divEl = useRef(null)
+  const videoEl = useRef(null)
   useEffect(() => {
     const script = document.createElement('script')
-    script.src =
-      'https://player.live-video.net/1.10.0/amazon-ivs-videojs-tech.min.js'
+    // script.src =
+    //   'https://player.live-video.net/1.10.0/amazon-ivs-videojs-tech.min.js'
+    script.src = 'https://player.live-video.net/1.0.0/amazon-ivs-player.min.js'
+    script.async = true
     document.body.appendChild(script)
-
-    script.addEventListener('load', () => {
-      const PLAYBACK_URL =
-        'https://e7f98e7e3456.ap-northeast-1.playback.live-video.net/api/video/v1/ap-northeast-1.861540815413.channel.DFdnCnWsjvNF.m3u8'
-
-      registerIVSTech(videojs, {} as PlayerConfig)
-
-      // Initialize player
-      const player = videojs(
-        'amazon-ivs-videojs',
-        {
-          techOrder: ['AmazonIVS'],
-        },
-        () => {
-          console.log('Player is ready to use!')
-
-          player.src(PLAYBACK_URL)
-        }
-      )
-
-      playerRef.current = player
-    })
+    script.onload = () => {
+      // eslint-disable-next-line no-undef,@typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      if (IVSPlayer.isPlayerSupported) {
+        // eslint-disable-next-line no-undef,@typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const player = IVSPlayer.create()
+        player.attachHTMLVideoElement(document.getElementById('video-player'))
+        player.load(
+          'https://e7f98e7e3456.ap-northeast-1.playback.live-video.net/api/video/v1/ap-northeast-1.861540815413.channel.DFdnCnWsjvNF.m3u8'
+        )
+        player.play()
+      }
+    }
 
     return () => {
-      // playerRef.current.dispose()
       document.body.removeChild(script)
     }
   }, [])
+  // document.body.appendChild(script)
+  //
+  // script.addEventListener('load', () => {
+  //   const PLAYBACK_URL =
+  //     'https://e7f98e7e3456.ap-northeast-1.playback.live-video.net/api/video/v1/ap-northeast-1.861540815413.channel.DFdnCnWsjvNF.m3u8'
+  //
+  //   registerIVSTech(videojs, {} as PlayerConfig)
+  //
+  //   // Initialize player
+  //   const player = videojs(
+  //     'amazon-ivs-videojs',
+  //     {
+  //       techOrder: ['AmazonIVS'],
+  //     },
+  //     () => {
+  //       console.log('Player is ready to use!')
+  //
+  //       player.src(PLAYBACK_URL)
+  //     }
+  //   )
+  //
+  //   playerRef.current = player
+  // };
+  // )
+
+  // return () => {
+  //   playerRef.current.dispose()
+  //   document.body.removeChild(script)
+  // }
+  // }, [])
 
   return (
-    <div>
+    <div ref={divEl}>
       <video
-        id="amazon-ivs-videojs"
-        className="video-js vjs-4-3 vjs-big-play-centered"
-        controls
-        autoPlay
+        id="video-player"
+        ref={videoEl}
         playsInline
-        muted
-        src="https://e7f98e7e3456.ap-northeast-1.playback.live-video.net/api/video/v1/ap-northeast-1.861540815413.channel.DFdnCnWsjvNF.m3u8"
-      ></video>
+        autoPlay
+        height={300}
+        controls
+      />
     </div>
   )
 }
+// <div>
+//   <div ref={playerRef}></div>
+//   <video
+//     id="amazon-ivs-videojs"
+//     className="video-js vjs-4-3 vjs-big-play-centered"
+//     controls
+//     autoPlay
+//     playsInline
+//     muted
+//     src="https://e7f98e7e3456.ap-northeast-1.playback.live-video.net/api/video/v1/ap-northeast-1.861540815413.channel.DFdnCnWsjvNF.m3u8"
+//   ></video>
+// </div>
+// );
+// }
 
 export default AmazonIVSPlayer
 
